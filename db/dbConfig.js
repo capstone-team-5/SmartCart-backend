868e9c573dfe5d0fea82b99c9fd1618d939b8352
Diandre Miller <millerdiandre@gmail.com>
2023-08-21 18:06:52 -0400

    added db config file

---
+++ db/dbConfig.js
@@ -0,0 +1,22 @@
+const pgp = require('pg-promise')();
+require('dotenv').config();
+
+const { DATABASE_URL } = process.env;
+
+const cn = DATABASE_URL 
+
+    ? {
+        connectionString: DATABASE_URL,
+        max: 30,
+    }
+
+    : {
+        host: process.env.PG_HOST,
+        port: process.env.PG_PORT,
+        database: process.env.PG_DATABASE,
+        user: process.env.PG_USER
+    }
+
+const db = pgp(cn);
+
+module.exports = db;
