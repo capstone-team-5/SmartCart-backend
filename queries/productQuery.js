f8672a97145566cac0e0d0a7b7109eee636c5405
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-08-23 05:52:33 -0400

    remove CRUD from productquery, replace id with product_id

---
+++ queries/productQuery.js
@@ -0,0 +1,32 @@
+// productQuery.js
+const db = require("../db/dbConfig.js");
+
+// get all Products
+const getAllProducts = async () => {
+  try {
+    const allProducts = await db.any(
+      "SELECT * FROM product ORDER BY product_id"
+    );
+    return allProducts;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// get one product
+const getOneProduct = async (id) => {
+  try {
+    const oneProduct = await db.oneOrNone(
+      "SELECT * FROM product WHERE product_id = $1",
+      id
+    );
+    return { result: oneProduct };
+  } catch (error) {
+    throw { error };
+  }
+};
+
+module.exports = {
+  getAllProducts,
+  getOneProduct,
+};
