4497719c3eb0fdce3e71e92f91e9f6b86ee0842a
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-09-08 06:14:01 -0400

    optimize code to fetch results in a single query

--- controllers/store_productController.js
+++ controllers/store_productController.js
@@ -10,15 +10,19 @@ comparePrices.get("/", async (req, res) => {
   const stores = {}; // This object will store the results by store
 
   try {
-    for (const productId of productArray) {
-      const results = await getComparisonResults(productId);
-      for (const result of results) {
-        const { store_id, product_price } = result;
-        if (!stores[store_id]) {
-          stores[store_id] = {};
-        }
-        stores[store_id][`product${productId}`] = product_price;
+    // Fetch results for all products in a single query
+    const results = await getComparisonResults(productArray);
+
+    for (const result of results) {
+      const { store_id, product_id, product_price } = result;
+
+      // Create a store entry if it doesn't exist
+      if (!stores[store_id]) {
+        stores[store_id] = {};
       }
+
+      // Use the product ID as the key in the store's entry
+      stores[store_id][product_id] = product_price;
     }
 
     // Calculate the total for each store
@@ -26,7 +30,6 @@ comparePrices.get("/", async (req, res) => {
       const store = stores[storeId];
       store.total = Object.values(store).reduce((acc, price) => acc + price, 0);
     }
-
     res.status(200).json({ stores });
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
--- queries/store_productQuery.js
+++ queries/store_productQuery.js
@@ -1,11 +1,11 @@
 const db = require("../db/dbConfig.js");
 
-// Get price information for a product across all stores
-const getComparisonResults = async (productId) => {
+// Get price information for multiple products across all stores
+const getComparisonResults = async (productIds) => {
   try {
     const results = await db.manyOrNone(
-      "SELECT store_id, product_price FROM store_product WHERE product_id = $1",
-      productId
+      "SELECT store_id, product_id, product_price FROM store_product WHERE product_id IN ($1:csv)",
+      [productIds]
     );
 
     return results;
