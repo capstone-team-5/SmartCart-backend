57d754419ab511a5e880dd1355b86128537cc09f
Diandre Miller <millerdiandre@gmail.com>
2023-09-10 21:58:45 -0400

    added images to seed

--- db/seed.sql
+++ db/seed.sql
@@ -2,16 +2,16 @@
 
 INSERT INTO store (store_name, store_address, store_city, store_state, store_zipCode, store_image, store_hours, store_accepts_EBT, store_phone_number)
 VALUES
-    ('City Fresh Market', '229 Knickerbocker Ave #2401', 'Brooklyn', 'NY', 11237, 'store1.jpg', 'Mon - Sat 24 hours', true, '(123) 456-7890'),
-    ('Mr. Lemon', '410 Knickerbocker Ave', 'Brooklyn', 'NY', 11237, 'store3.jpg', '7:00 AM - 11:00 PM', false, '(987) 654-3210'),
-    ('bp', '1379 Halsey St', 'Brooklyn', 'NY', 11237, 'store5.jpg', 'Mon-Fri: 8am-10pm, Sat-Sun: 9am-9pm', true, '(789) 012-3456'),
-    ('Buena Vista Deli & Grocery', '152 Knickerbocker Ave', 'Brooklyn', 'NY', 11237, 'store2.jpg', '9:00 AM - 9:00 PM', true, '(555) 123-4567'),
-    ('EUROPE', '307 Brighton Beach Ave', 'Brooklyn', 'NY', 11235, 'store8.jpg', '8:00 AM - 8:00 PM', true, '(222) 999-7777'),
-    ('Atlantis', '1127 Flushing Ave', 'Brooklyn', 'NY', 11237, 'store4.jpg', '10:00 AM - 8:00 PM', true, '(246) 135-7900'),
-    ('Valery Deli Grocery Store', '214 Knickerbocker Ave', 'Brooklyn', 'NY', 11237, 'store7.jpg', '7:30 AM - 9:30 PM', true, '(888) 555-1212'),
-    ('Clasico Grocery', '343 Knickerbocker Ave', 'Brooklyn', 'NY', 11237, 'store9.jpg', '9:00 AM - 7:00 PM', true, '(444) 777-3333'),
-    ('To The World Farm', '655 Grand St #4854', 'Brooklyn', 'NY', 11211, 'store10.jpg', '10:30 AM - 6:30 PM', false, '(777) 111-3333'),
-    ('Williams Avenue Community Garden', '88 Williams Ave', 'Brooklyn', 'NY', 11207, 'store6.jpg', '6:00 AM - 12:00 AM', false, '(321) 654-9870');
+    ('City Fresh Market', '229 Knickerbocker Ave #2401', 'Brooklyn', 'NY', 11237, 'https://www.cityfreshmarket.com/wp_2013/wp-content/uploads/2021/08/City-Fresh-Market_store-exterior.jpg', 'Mon - Sat 24 hours', true, '(123) 456-7890'),
+    ('Mr. Lemon', '410 Knickerbocker Ave', 'Brooklyn', 'NY', 11237, 'https://db535g4onu9gg.cloudfront.net/eyJidWNrZXQiOiJtZXJjYXRvLWltYWdlcyIsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJmaWxsIiwid2lkdGgiOjM5MCwiaGVpZ2h0IjozOTB9fSwia2V5Ijoic3RvcmUtaW1hZ2VzLzRhSnlQaVFGT0RaQTJEa0ZjeTB0R1plemVyaGpzaWF0VGp0eW0yckQuanBnIn0=', '7:00 AM - 11:00 PM', false, '(987) 654-3210'),
+    ('bp', '1379 Halsey St', 'Brooklyn', 'NY', 11237, 'https://bloximages.chicago2.vip.townnews.com/moraminn.com/content/tncms/assets/v3/editorial/c/4b/c4bf0a28-c630-11ec-9790-63a59f47cc38/62694a51438ea.image.jpg?resize=400%2C200', 'Mon-Fri: 8am-10pm, Sat-Sun: 9am-9pm', true, '(789) 012-3456'),
+    ('Buena Vista Deli & Grocery', '152 Knickerbocker Ave', 'Brooklyn', 'NY', 11237, 'https://media-cdn.tripadvisor.com/media/photo-s/17/6a/4f/10/buena-vista-market.jpg', '9:00 AM - 9:00 PM', true, '(555) 123-4567'),
+    ('EUROPE', '307 Brighton Beach Ave', 'Brooklyn', 'NY', 11235, 'https://l450v.alamy.com/450v/2hg6c26/brighton-beach-brooklyn-new-york-city-new-york-usa-november-6-2021-market-selling-european-foods-in-brooklyn-2hg6c26.jpg', '8:00 AM - 8:00 PM', true, '(222) 999-7777'),
+    ('Atlantis', '1127 Flushing Ave', 'Brooklyn', 'NY', 11237, 'https://s3-media0.fl.yelpcdn.com/bphoto/FtRIzIYwZm9ELWJjd-ZcYA/348s.jpg', '10:00 AM - 8:00 PM', true, '(246) 135-7900'),
+    ('Valery Deli Grocery Store', '214 Knickerbocker Ave', 'Brooklyn', 'NY', 11237, 'https://lh3.googleusercontent.com/p/AF1QipNveMLhM6XARij34NCdDN1hz2OOE9EiCmu3ERVa=w1080-h608-p-no-v0', '7:30 AM - 9:30 PM', true, '(888) 555-1212'),
+    ('Clasico Grocery', '343 Knickerbocker Ave', 'Brooklyn', 'NY', 11237, 'https://fastly.4sqi.net/img/general/600x600/52267008_Ysly7MFq4edyrFJKhMH3-q_KjqPkKW0UR9WXdlEKTRk.jpg', '9:00 AM - 7:00 PM', true, '(444) 777-3333'),
+    ('To The World Farm', '655 Grand St #4854', 'Brooklyn', 'NY', 11211, 'https://ir.4sqi.net/img/general/original/XQ55DNA0OJ25DMNN40BPAECWKQSZ10LZSJK1FCRUZBCB1QSP.jpg', '10:30 AM - 6:30 PM', false, '(777) 111-3333'),
+    ('Williams Avenue Community Garden', '88 Williams Ave', 'Brooklyn', 'NY', 11207, 'https://williams.mapseed.org/static/css/images/Willams-Ave-BK.jpg', '6:00 AM - 12:00 AM', false, '(321) 654-9870');
 
 INSERT INTO product (store_id, product_name, product_image, product_description, product_category, product_weight, product_price, product_discount, product_unit, product_brand, product_allergens, product_special_diet, product_health_label, product_is_fresh, product_is_frozen, product_is_halal, product_is_kosher, product_is_non_processed, product_is_organic, product_is_vegetarian, product_is_stock, product_is_vegan, product_is_seafood)
 VALUES
