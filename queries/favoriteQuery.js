769463657be6ff8a7c643a2f31a296fe0d3fe28b
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-10-09 11:12:59 -0400

    change in fav query

--- queries/favoriteQuery.js
+++ queries/favoriteQuery.js
@@ -4,9 +4,16 @@ const db = require("../db/dbConfig.js");
 const getFavoritesByShopperId = async (shopperId) => {
   try {
     const favorites = await db.manyOrNone(
-      "SELECT * FROM favorite WHERE shopper_firebase_uid = $1",
+      // "SELECT * FROM favorite WHERE shopper_firebase_uid = $1",
+      // shopperId
+
+      "select product_name, product_image from product, favorite, shopper where \
+      favorite.product_id = product.product_id and \
+      shopper.shopper_firebase_uid = favorite.shopper_firebase_uid and shopper.shopper_firebase_uid = $1",
       shopperId
     );
+
+    console.log(favorites);
     return { result: favorites };
   } catch (error) {
     console.log(error);
@@ -22,7 +29,6 @@ const addOneFavorite = async (favorite) => {
       "INSERT INTO favorite (shopper_firebase_uid, product_id) VALUES ($1, $2) RETURNING *",
       [favorite.shopper_firebase_uid, favorite.product_id]
     );
-    console.log("newFavorite:", newFavorite);
     return { result: newFavorite };
   } catch (error) {
     console.log("error", error);
