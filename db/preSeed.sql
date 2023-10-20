9ef1e1870d545605db30d401c8d6c39bf7e71475
Diandre Miller <millerdiandre@gmail.com>
2023-10-19 20:32:55 -0400

    added data to seed file, created file to seed data before entering in seed file

---
+++ db/preSeed.sql
@@ -0,0 +1,46 @@
+-- to check values before they are put into the seed file
+
+INSERT INTO product (product_name, product_image, product_description, product_category, product_weight, product_unit, product_brand, product_is_tree_nuts_free, product_is_egg_free, product_is_fresh, product_is_frozen, product_is_halal, product_is_kosher, product_is_non_processed, product_is_organic, product_is_vegetarian, product_is_stock, product_is_vegan_friendly, product_is_seafood, product_is_peanut_free, product_is_caffeine, product_is_dairy_free, product_is_no_preservatives, product_is_no_added_sugar, product_is_nuts_free, product_is_keto_friendly, product_is_low_carb_diet, product_is_gluten_free, product_is_international) 
+VALUES
+
+('Rani Ghee', 'https://m.media-amazon.com/images/I/71GIQW73Z-L._AC_UL640_FMwebp_QL65_.jpg', 'Ghee for cooking and flavoring', 'Dairy', 250, 'grams', 'Rani', true, true, true, false, true, true, true, true, true, true, false, false, true, false, false, true, true, true, true, true, true, true),
+
+            
+('Indian Head Yellow Cornmeal', 'https://m.media-amazon.com/images/I/91-lkSyPbVL._AC_UL640_FMwebp_QL65_.jpg', 'Yellow cornmeal for cooking and baking', 'Grains', 500, 'grams', 'Indian Head', true, true, true, false, true, true, true, false, true, true, false, false, false, true),
+('Okra', 'https://m.media-amazon.com/images/I/51wPo-4ITgL._AC_UL640_FMwebp_QL65_.jpg', 'Fresh okra', 'Vegetables', 300, 'grams', 'Generic', true, true, true, false, true, true, true, true, true, true, false, false, true, false),
+('Tidal Flying Fish', 'https://www.shopmassystoresbb.com/wp-content/uploads/2021/03/0075568100551.jpg', 'Flying fish, a seafood product', 'Seafood', 400, 'grams', 'Tidal', true, true, true, false, true, true, true, false, true, true, true, false, true, true, false),
+('Barilla Macaroni Elbows', 'https://m.media-amazon.com/images/I/613m71B10sL._AC_UL640_FMwebp_QL65_.jpg', 'Macaroni elbows for pasta dishes', 'Pasta & Noodles', 500, 'grams', 'Barilla', true, true, true, false, true, true, true, false, true, true, false, false, false, true),
+('Goya Black Beans', 'https://m.media-amazon.com/images/I/81S7qCera6L._AC_UL640_FMwebp_QL65_.jpg', 'Canned black beans', 'Canned Goods', 400, 'grams', 'Goya', true, true, true, false, true, true, true, false, true, true, false, false, false, true);
+
+
+
+
+
+
+
+('Del Monte Diced Tomaotes & Green Chillis', 'https://m.media-amazon.com/images/I/81HyVTBNO6L._AC_UL640_FMwebp_QL65_.jpg', 'Canned tomatoes with green chilies', 'Canned Goods', 400, 'grams', 'Del Monte', true, true, true, false, true, true, true, true, true, true, false, false, false, true, false, true, false, true, false, false, false),
+('Applegate Ground Pork', 'https://m.media-amazon.com/images/I/81Z4bvm0CKL._AC_UL640_FMwebp_QL65_.jpg', 'Ground pork for cooking', 'Meat & Poultry', 500, 'grams', 'Applegate', true, true, false, true, true, true, true, false, false, false, false, false, false, true, false, true, false, false, false, false),
+('Progresso Breadcrumbs', 'https://m.media-amazon.com/images/I/8166bkyP2rL._AC_UL640_FMwebp_QL65_.jpg', 'Breadcrumbs for cooking and baking', 'Baking', 250, 'grams', 'Progresso', true, true, true, false, true, true, true, false, true, true, false, false, false, true, false, true, true, false, false, false, false),
+('Hunts Tomato Sauce', 'https://m.media-amazon.com/images/I/71zNyT0CT9L._AC_UL640_FMwebp_QL65_.jpg', 'Tomato sauce for cooking', 'Canned Goods', 250, 'milliliters', 'Hunts', true, true, true, false, true, true, true, true, true, true, false, false, false, true, false, true, false, false, false, true, false),
+('Hunts Crushed Tomatoes', 'https://m.media-amazon.com/images/I/71lMn5Otr4L._AC_UL640_FMwebp_QL65_.jpg', 'Crushed tomatoes for cooking', 'Canned Goods', 400, 'grams', 'Hunts', true, true, true, false, true, true, true, true, true, true, false, false, false, true, false, true, false, false, false, true, false),
+('McCormick Dried Basil', 'https://m.media-amazon.com/images/I/91USWlRBlxL._AC_UL640_FMwebp_QL65_.jpg', 'Dried basil for seasoning', 'Spices', 30, 'grams', 'McCormick', true, true, true, false, true, true, true, true, true, true, false, false, false, true, false, true, false, false, false, false, true),
+('McCormick Dried Oregano', 'https://m.media-amazon.com/images/I/81G-epOwdHS._AC_UL640_FMwebp_QL65_.jpg', 'Dried oregano for seasoning', 'Spices', 30, 'grams', 'McCormick', true, true, true, false, true, true, true, true, true, true, false, false, false, true, false, true, false, false, false, false, true),
+('Fleischmanns ActiveDry Yeast', 'https://m.media-amazon.com/images/I/71VVfr4oeUL._AC_UL640_FMwebp_QL65_.jpg', 'Active dry yeast for baking', 'Baking', 30, 'grams', 'Fleischmanns', true, true, true, false, true, true, true, true, true, true, false, false, false, true, false, true, true, false, false, false, false),
+('Pillsbury Refrigerated Pie Crust', 'https://m.media-amazon.com/images/I/81FbXM0rcAL._AC_UL640_FMwebp_QL65_.jpg', 'Refrigerated pie crust for baking', 'Baking', 400, 'grams', 'Pillsbury', true, true, true, false, true, true, true, true, true, true, false, false, false, true, false, true, true, false, false, false, false),
+('Ocean Spray Craisins Dried Cranberry', 'https://m.media-amazon.com/images/I/81pCsM6sAYL._AC_UL640_FMwebp_QL65_.jpg', 'Dried cranberries for snacking', 'Snacks', 250, 'grams', 'Ocean Spray', true, true, true, false, true, true, true, true, true, true, false, false, false, true, false, true, false, false, true, false, false),
+('Rold Gold Pretzel Sticks', 'https://m.media-amazon.com/images/I/81+P77G35nL._AC_UL640_FMwebp_QL65_.jpg', 'Pretzel sticks for snacking', 'Snacks', 250, 'grams', 'Rold Gold', true, true, true, false, true, true, true, true, true, true, false, false, false, true, false, true, false, false, true, false, false),
+('David Sunflower Seeds', 'https://target.scene7.com/is/image/Target/GUEST_1953bedc-7bf8-4441-b3fe-8717efdff7ab?wid=1200&hei=1200&qlt=80&fmt=webp', 'Roasted sunflower seeds for snacking', 'Snacks', 150, 'grams', 'David', true, true, true, false, true, true, true, true, true, true, false, false, false, true, false, true, false, false, true, false, false),
+('M&Ms Milk Chocolates', 'https://target.scene7.com/is/image/Target/GUEST_bf6aeb98-6ea3-4be2-81d5-17088f7eca15?wid=1200&hei=1200&qlt=80&fmt=webp', 'Milk chocolate candies', 'Snacks', 200, 'grams', 'M&Ms', true, true, true, false, true, true, true, true, true, true, false, false, false, true, false, true, false, false, true, false, false),
+('Hersheys Milk Chocolate Bar', 'https://target.scene7.com/is/image/Target/GUEST_21b4ff8f-83f4-4787-bcfc-a9d6b39323a8?qlt=85&fmt=webp&hei=325&wid=325', 'Milk chocolate bar', 'Snacks', 50, 'grams', 'Hersheys', true, true, true, false, true, true, true, true, true, true, false, false, false, true, false, true, false, false, true, false, false),
+('Kraft Jet-Puffed Marshmallows', 'https://m.media-amazon.com/images/I/516ZBpVHsoL._AC_UL640_FMwebp_QL65_.jpg', 'Marshmallows for snacking and baking', 'Snacks', 400, 'grams', 'Kraft', true, true, true, false, true, true, true, true, true, true, false, false, false, true, false, true, false, false, true, false, false),
+('Orville Redenbachers Popcorn Kernels', 'https://m.media-amazon.com/images/I/81m-pDwzXYL._AC_UL640_FMwebp_QL65_.jpg', 'Popcorn kernels for popping', 'Snacks', 250, 'grams', 'Orville Redenbachers', true, true, true, false, true, true, true, true, true, true, false, false, false, true, false, true, false, false, true, false, false),
+('Jolly Ranchers Hard Candies', 'https://m.media-amazon.com/images/I/81JvLHUxAfL._AC_UL640_FMwebp_QL65_.jpg', 'Hard fruit-flavored candies', 'Snacks', 200, 'grams', 'Jolly Ranchers', true, true, true, false, true, true, true, true, true, true, false, false, false, true, false, true, false, false, true, false, false),
+('Ghirardelli White Chocolate', 'https://m.media-amazon.com/images/I/51FLRA4dkWL._AC_UL640_FMwebp_QL65_.jpg', 'White chocolate for baking', 'Baking', 100, 'grams', 'Ghirardelli', true, true, true, false, true, true, true, false, true, false, false, false, false, true, false, true, false, false, false, false, true),
+('Bobs Red Mills Unsweetened Coconut Flakes', 'https://cloudinary.images-iherb.com/image/upload/f_auto,q_auto:eco/images/brm/brm03581/v/6.jpg', 'Unsweetened coconut flakes', 'Baking', 200, 'grams', 'Bobs Red Mills', true, true, true, false, true, true, true, true, true, true, false, false, false, true, false, true, false, false, false, true, false),
+('Grey Poupin Dijon Mustard', 'https://m.media-amazon.com/images/I/61clohtWkdL._AC_UL640_FMwebp_QL65_.jpg', 'Dijon mustard for cooking', 'Condiments', 100, 'milliliters', 'Grey Poupin', true, true, true, false, true, true, true, false, true, true, false, false, false, true, false, true, false, true, false, false, false),
+('Aunt Jemima', 'https://m.media-amazon.com/images/I/81AqCl8Lf5L._SX679_.jpg', 'Pancake syrup', 'Condiments', 400, 'milliliters', 'Aunt Jemima', true, true, true, false, true, true, true, true, true, true, false, false, false, true, false, true, false, true, false, false, false);
+
+
+
+
+
